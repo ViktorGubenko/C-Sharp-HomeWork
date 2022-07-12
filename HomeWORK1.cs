7f243ccb890efed81472ccf912dc94a556ec640b
Victor.Gubenko <boge01@yandex.ru>
2022-07-12 23:57:57 +0300

    Complete first Homework

---
+++ HomeWORK1.cs
@@ -0,0 +1,40 @@
+﻿/* Task 1
+Console.WriteLine("Input integer number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input integer number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2) Console.WriteLine (num1);
+if (num2 > num1) Console.WriteLine (num2);
+*/
+
+/* Task 2
+Console.WriteLine("Input integer number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input integer number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input integer number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 & num1 > num3) Console.WriteLine (num1);
+if (num2 > num1 & num2> num3) Console.WriteLine (num2);
+if (num3 > num2 & num3> num1) Console.WriteLine (num3);
+*/
+
+/* Task 3
+Console.WriteLine("Input integer number ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0) Console.WriteLine ("четное");
+else Console.WriteLine ("нечетное");
+*/
+
+/* Task 4
+Console.WriteLine("Input integer number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = (1);
+
+while (current <= num)
+{
+    if (current % 2 == 0) Console.Write (current + " ");
+    current++;
+}
+*/
